baf6d9a13529637946cc3f1d7e4b3a103d99921d
devante11 <devante2700@gmail.com>
2021-02-21 23:16:25 -0500

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import DataRequired, Email
 
 
 class ContactForm(FlaskForm):
-	name= TextField('Name' validators=[DataRequired('A name is required!')])
-	email= TextField('Email Address'validators=[DataRequired('A email Address is required!')])
-	subject= TextField('Subject'validators=[DataRequired('A subject is required!')])
+	name= SpringField('Name' validators=[DataRequired('A name is required!')])
+	email= SpringField('Email Address'validators=[DataRequired('A email Address is required!')])
+	subject= SpringField('Subject'validators=[DataRequired('A subject is required!')])
 	message= TextAreaField('Message' validators=[DataRequired('A message is required')])
 	submit= Submifield('send')
